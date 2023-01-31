f4dd9a5575038881ab83e780392079eb9e1ae45e
Reggie188 <mr.tulyakov92@gmail.com>
2023-02-01 00:21:14 +0300

    Add 8 lesson HW

--- cs_hw_4/less_pract/Program.cs
+++ cs_hw_4/less_pract/Program.cs
@@ -1,55 +1,56 @@
 ﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
 
-void revBinSys(int num)
+/*float factorial(int n)
 {
-    int tempNum = 0;
-    string binNum = "";
-    if(num != 0)  
+    int x = 1;
+    for (int i = 1; i <= n; i++)
     {
-        tempNum = num % 2;
-        binNum += tempNum;
-        revBinSys(num /= 2);
+        x *= i;
     }
-    Console.Write(binNum);
+    return x;
 }
 
-bool inpValid(string num)
+
+
+Console.WriteLine("Введите нужное количество строк треугольника Паскаля:");
+string s = Console.ReadLine();
+int n = Convert.ToInt32(s);
+
+for(int i = 0; i < n; i++)
 {
-    bool res_val = int.TryParse(num, out int res_num);
-    if(res_val == true)
+    for (int c = 0; c <= (n - i); c++) // создаём после каждой строки n-i отступов от левой стороны консоли, чем ниже строка, тем меньше отступ
     {
-        return true;
-    }   
-    else
-    {
-        return false;
+        Console.Write($" "); 
     }
+    for (int c = 0; c <= i; c++)
+    {
+        Console.Write($" "); // создаём пробелы между элементами треугольника
+        Console.Write(factorial(i) / (factorial(c) * factorial(i - c))); //формула вычисления элементов треугольника
+    }
+    Console.WriteLine(); // после каждой строки с числами отступаем две пустые строчки
 }
 
-int intUsrNum = 0;
-string strUserNum = "";
-while(true)
+Console.WriteLine("Rows count: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+ 
+for (int y = 0; y < rows; y++)
 {
-    Console.Write("\n");
-    Console.WriteLine("Enter user num or enter 'q' for stop: ");
-    strUserNum = Console.ReadLine();
-    if(strUserNum.ToLower() == "q")
+    int c = 1;
+    for (int q = 0; q < rows - y; q++)
     {
-        Console.WriteLine("Bye!");
-        break;
+        System.Console.Write(" ");
     }
-    else
+
+    for (int x = 0; x <= y; x++)
     {
-        bool result_01 = inpValid(strUserNum);
-        if(result_01 == true)
-        {
-            intUsrNum = Int32.Parse(strUserNum);
-        }
-        else
-        {
-            Console.WriteLine("Error enter!");
-            continue;
-        }
-    revBinSys(intUsrNum);
+        System.Console.Write(" {0:D}", c);
+        c = c * (y - x) / (x + 1);
     }
-}
+    System.Console.WriteLine();
+}*/
+
+int[][] jaggedArray = new int[3][];
+
+jaggedArray[0] = new int[5];
+
+Console.Write(jaggedArray[0]);
